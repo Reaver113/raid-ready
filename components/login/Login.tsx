91e8b915545c8ab3d5bfba6c9fa846c80def4e10
Reaver113 <104881727+Reaver113@users.noreply.github.com>
2026-01-29 21:26:34 +1000

    added loadingwheel

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -3,21 +3,34 @@
 import Container from "react-bootstrap/Container";
 import styles from "./login.module.css";
 import Button from "@/components/shared/button/Button";
+import LoadingWheel from "@/components/shared/loadingWheel/LoadingWheel";
+import { useState } from "react";
 import Heading from "../heading/Heading";
 import { signIn } from "next-auth/react";
 
 const Login = () => {
-  const handleClick = () => {
-    signIn("battlenet");
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    try {
+      setLoading(true);
+      await signIn("battlenet");
+    } catch (e) {
+      setLoading(false);
+    }
   };
   return (
     <Container className={styles.loginContainer}>
       <Heading />
-      <Button
-        onClick={handleClick}
-        className={styles.Button}
-        label={`Login to Continue`}
-      />
+      {loading ? (
+        <LoadingWheel />
+      ) : (
+        <Button
+          onClick={handleClick}
+          className={styles.Button}
+          label={`Login to Continue`}
+        />
+      )}
     </Container>
   );
 };
