dd50ae0d9f954b526d668e4603f69b38c209e15d
Reaver113 <104881727+Reaver113@users.noreply.github.com>
2026-01-29 21:29:45 +1000

    removed props

--- components/userPanel/UserPanel.tsx
+++ components/userPanel/UserPanel.tsx
@@ -5,19 +5,11 @@ import { useAuth } from "@/hooks/useAuth";
 import Bubble from "../shared/bubble/Bubble";
 import styles from "./user-panel.module.css";
 
-const UserPanel: React.FC<ColumnProps> = ({ xs, sm, md, lg, xl }) => {
+const UserPanel = () => {
   const { session } = useAuth();
 
   return (
-    <Bubble
-      type="solid"
-      xs={xs}
-      sm={sm}
-      md={md}
-      lg={lg}
-      xl={xl}
-      className={styles.userPanel}
-    >
+    <Bubble type="solid" xs={12} className={styles.userPanel}>
       <h2 className={styles.userName}>Welcome {session?.user?.name}</h2>
     </Bubble>
   );
