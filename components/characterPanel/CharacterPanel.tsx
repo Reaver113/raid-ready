5db17cce57cf2a49822e15a3f19063529462edff
Reaver113 <104881727+Reaver113@users.noreply.github.com>
2026-01-29 18:11:22 +1000

    loading wheel added, states split

--- components/characterPanel/CharacterPanel.tsx
+++ components/characterPanel/CharacterPanel.tsx
@@ -6,21 +6,44 @@ import { Col, Row } from "react-bootstrap";
 import fetchProfile from "@/fetch/fetchProfile";
 import CharacterSelect from "./characterSelect/CharacterSelect";
 import CharacterInfo from "./characterInfo/CharacterInfo";
+import fetchEquipment from "@/fetch/fetchEquipment";
+import LoadingWheel from "@/components/shared/loadingWheel/LoadingWheel";
 
 export default function CharacterPanel() {
   const [profile, setProfile] = useState<WoWProfile | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+  const [profileLoading, setProfileLoading] = useState(true);
+  const [characterLoading, setCharacterLoading] = useState(false);
+  const [characterError, setCharacterError] = useState<string | null>(null);
+  const [profileError, setProfileError] = useState<string | null>(null);
   const [selectedRealm, setSelectedRealm] = useState<string | null>(null);
   const [selectedCharacter, setSelectedCharacter] = useState<Character | null>(
     null,
   );
+  const [characterEquipment, setCharacterEquipment] = useState<any>(null);
 
   useEffect(() => {
-    fetchProfile({ setLoading, setProfile, setError });
+    fetchProfile({
+      setLoading: setProfileLoading,
+      setProfile,
+      setError: setProfileError,
+    });
   }, []);
 
+  useEffect(() => {
+    if (selectedCharacter) {
+      fetchEquipment({
+        setLoading: setCharacterLoading,
+        setCharacterEquipment,
+        setError: setCharacterError,
+        realmSlug: selectedCharacter?.realm.slug || "",
+        characterName: selectedCharacter?.name.toLowerCase() || "",
+      });
+    }
+  }, [selectedCharacter]);
+
   console.log(selectedCharacter);
+
+  console.log(characterEquipment);
 
   // Get all unique realms from the profile
   const allCharacters =
@@ -36,9 +59,14 @@ export default function CharacterPanel() {
     ? allCharacters.filter((char) => char.realm.slug === selectedRealm)
     : [];
 
-  if (loading)
-    return <div className={styles.loadingText}>Loading profile...</div>;
-  if (error) return <div className={styles.errorText}>Error: {error}</div>;
+  if (profileLoading)
+    return (
+      <Col xs={12} className={styles.loading}>
+        <LoadingWheel />
+      </Col>
+    );
+  if (profileError)
+    return <div className={styles.errorText}>Error: {profileError}</div>;
   if (!profile)
     return <div className={styles.noDataText}>No profile data available</div>;
 
