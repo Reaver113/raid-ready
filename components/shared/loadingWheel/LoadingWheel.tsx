e7c30465ad841ce6bcc8b1456a37fc4ea57769e1
Reaver113 <104881727+Reaver113@users.noreply.github.com>
2026-01-30 22:24:30 +1000

    front end updates

--- components/shared/loadingWheel/LoadingWheel.tsx
+++ components/shared/loadingWheel/LoadingWheel.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "./loading-wheel.module.css";
 
-const LoadingWheel: React.FC = () => (
+const LoadingWheel: React.FC<{ size?: number }> = ({ size }) => (
   <div className={styles.container} role="status" aria-label="Loading">
-    <div className={styles.spinner} />
+    <div
+      className={styles.spinner}
+      style={{ height: size ?? "60px", width: size ?? "60px" }}
+    />
   </div>
 );
 
